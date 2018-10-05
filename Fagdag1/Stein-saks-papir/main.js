5cfead92e11e2dcf448c309bdaf202a271aeeae3
mareis <mads.reistadbakk@mrfylke.no>
2018-10-05 13:39:46 +0200

    ,

--- Fagdag1/Stein-saks-papir/main.js
+++ Fagdag1/Stein-saks-papir/main.js
@@ -1,1 +1,36 @@
-document.body.style.backgroundColor = '#5f5f5f';
+let bilderSpiller = document.querySelectorAll('.spiller');
+let bildeMaskin = document.querySelector('#maskin');
+let poengSpillerE = document.querySelector('#poengSpiller');
+let poengMaskinE = document.querySelector('#poengSpiller');
+
+let bilderMaskin = ['maskin_stein.png', 'maskin_saks.png', 'maskin_papir.png'];
+
+let poengSpiller = 0;
+let poengMaskin = 0;
+let stein = 0;
+let saks = 1;
+let papir = 2;
+
+bilderSpiller.forEach(klikkhandling);
+
+function klikkhandling(bilde, valgSpiller){
+    bilde.addEventListener('click', function () {
+        let valgMaskin = Math.floor(Math.random()*3);
+        bildeMaskin.src = `bilder/${bilderMaskin[valgMaskin]}`;
+
+        if(valgSpiller === valgMaskin){
+            console.log('uavgjort');
+        }
+        else if(valgSpiller === stein && valgMaskin === saks ||
+                valgSpiller === saks && valgMaskin === papir ||
+                valgSpiller === papir && valgMaskin === stein ){
+            poengSpiller++;
+            poengSpillerE.innerHTML = poengSpiller;
+        }
+        else{
+            poengMaskin++;
+            poengMaskinE.innerHTML = poengMaskin;
+
+        }
+    })
+}
